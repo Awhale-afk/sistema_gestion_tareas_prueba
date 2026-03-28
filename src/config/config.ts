8c823183db00a63407dd475c187b64c798fde80b
Emmanuel <est.emmgarcia.edu.co@gmail.com>
2026-03-28 16:59:56 -0500

    Cambios de etiquetas @  para mejor organización en JSDoc

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,14 @@
-/**@module Configuración */
+/**@module Configuracion */
 
-/** @ignore 
- * Busca credenciales, API keys, entre otras cosas del .env// Se importa la librería dotenv desde el archivo dotenv de node_modules*/
+
+//Busca credenciales, API keys, entre otras cosas del .env// Se importa la librería dotenv desde el archivo dotenv de node_modules*/
 import dotenv from 'dotenv'; 
 
 
 /**La clase Config establece que datos se deben buscar y que de que tipo son. 
 Están programados para SOLO lectura, no se pueden modificar por fuera de la clase
-@class Config
-@memberof module:Configuración*/
+@class 
+*/
 
 class Config{
     
@@ -16,23 +16,23 @@ class Config{
     /**Configuración del puerto del servidor 
      * @readonly
      * @type {number}
-     * @instance
-     * @memberof Config
+     * @public
+     * @memberof module:Configuracion
      */
     public readonly PORT: number;
     /**Configuración de la dirección URL de la base de datos 
      * @readonly
      * @type {string}
-     * @instance
-     * @memberof Config
+     * @public
+     * @memberof module:Configuracion
      */
     public readonly DB_URL: string;
     /**Configuración del token 
      * @readonly
      * @type {string}
-     * @instance
-     * @memberof Config
-     */
+     * @public
+     * @memberof module:Configuracion
+    */
     public readonly TOKEN: string;
 
     private constructor(){
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,25 @@
 /**@module Servidor */
-/**@ignore 
- * Importa configuraciones de node modules/@types (Es decir @types/express @types/node @types/cors) 
+
+/* Importa configuraciones de node modules/"@types" (Es decir @types/express @types/node @types/cors) 
 como "traductores" para Typescript.
 Viene de node_modules/@types/express/index.ts/linea 42-44.
 Se puede acceder a ellos con Ctrl + click en los parámetros {}*/
 import express, {Application, Request, Response} from 'express'; 
 
-/**@ignore 
- * Importa configuraciones sobre las conexiones para tener control sobre peticiones HTTP que vengan de un lugar diferente al que tiene la API (Puerto, Dominio url y protocolo (http))*/
+//Importa configuraciones sobre las conexiones para tener control sobre peticiones HTTP que vengan de un lugar diferente al que tiene la API (Puerto, Dominio url y protocolo (http))*/
 import cors from 'cors';        
-/**@ignore 
- * Se importa la constante config que contiene la instancia (objeto) de la clase Config en el archivo config.ts */
+
+ // Se importa la constante config que contiene la instancia (objeto) de la clase Config en el archivo config.ts */
 import {config} from './config/config.js' 
 const app: Application = express();
 const PORT = config.PORT;
 
 app.use(express.json());
-/**Pemite las peticiones HTTP de lugares externos a la API*/
+/**Pemite las peticiones HTTP de lugares externos a la API
+ * @public
+ * @function
+ * @memberof module:Servidor
+*/
 app.use(cors());
 
 
