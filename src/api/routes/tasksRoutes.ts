166eb3be4fa3f38ca552e0e64190bd043d330648
Emmanuel <est.emmgarcia.edu.co@gmail.com>
2026-03-29 16:01:58 -0500

    Descripción con swagger para la documentación de las rutas

--- src/api/routes/tasksRoutes.ts
+++ src/api/routes/tasksRoutes.ts
@@ -42,21 +42,21 @@ router.post('/createTask', createTask)
 /**
  * @swagger
  * /api/tasks/{userId}:
- * ..get:
- * ....summary: Obtener todas las tareas de un usuario
- * ....tags: [Tareas]
- * ....parameters:
- * ......- in: path
- * ........name: userId
- * ........required: true
- * ........schema:
- * ..........type: integer
- * ........description: ID del usuario (ej. 6)
- * ....responses:
- * ......200:
- * ........description: Lista de tareas encontrada
- * ......500:
- * ........description: Error del servidor
+ *  get:
+ *    summary: Obtener todas las tareas de un usuario
+ *    tags: [Tareas]
+ *    parameters:
+ *      - in: path
+ *      name: userId
+ *      required: true
+ *      schema:
+ *      type: integer
+ *      description: ID del usuario (6)
+ *    responses:
+ *         200:
+ *      description: Lista de tareas encontrada
+ *         500:
+ *      description: Error del servidor
  */
 
 router.get('/:user_id', seeTask);
